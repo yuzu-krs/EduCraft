3a59490bf4b3f14ca84d22e418a0856e62a687a4
yuzu-krs <yuzuto.poi@gmail.com>
2024-07-14 12:31:33 +0900

    GUIからCドライブ直下のEduCraft/main.をVSCodeで開くようにした。ファイルやフォルダがない場合は作成する。

--- src/minecraft/me/gamrboy4life/paradox/module/editor/VSCode.java
+++ src/minecraft/me/gamrboy4life/paradox/module/editor/VSCode.java
@@ -1,17 +1,82 @@
 package me.gamrboy4life.paradox.module.editor;
 
+import java.io.File;
+import java.io.IOException;
+
 import me.gamrboy4life.paradox.module.Category;
 import me.gamrboy4life.paradox.module.Module;
 
 public class VSCode extends Module{
+	
+	private boolean hasToggled=false;
+	
 	public VSCode() {
 		super("VSCode",0,Category.Editor);
 	}
 	
 	@Override
 	public void onUpdate() {
-		if(this.isToggled()) {
+		if(this.isToggled()&&!hasToggled) {
+			
+			//ここに処理を書く
+			openFileInVSCode("C:/EduCraft/main.c");
+			
+			//処理が完了した後、フラグをtrueに設定
+			hasToggled=true;
+			
+			//モジュールのトグルを解除
+			toggled=false;
 			
 		} 
 	}
+	
+	private void openFileInVSCode(String filePath) {
+		try {
+    		File file=new File(filePath);
+    		
+    		//ファイルのディレクトリが存在しない場合は作成する
+    		File parentDir=file.getParentFile();
+    		if(parentDir!=null&&!parentDir.exists()) {
+    			if(parentDir.mkdirs()) {
+    				System.out.println("ディレクトリを新規作成しました:"+parentDir.getPath());
+    			}
+    		}
+    		
+    		//ファイルが存在しない場合は新規作成
+    		if(!file.exists()) {
+    			if(file.createNewFile()) {
+    				System.out.println("ディレクトリを新規作成しました:"+parentDir.getPath());
+    			}
+    		}
+    		
+    		//VSCodeを起動し、指定のファイルを開く
+    		ProcessBuilder pb=new ProcessBuilder("cmd.exe","/c","code",filePath);
+    		pb.start();
+    		
+    		System.out.println("VSCodeでファイルを開きます: "+filePath);
+    		
+    		
+    		
+    	}catch(IOException e){
+    		e.printStackTrace();
+    	}
+	}
+	
+	@Override
+	public void onEnable() {
+		super.onEnable();
+		hasToggled=false;
+	}
+	
+	@Override
+	public void onDisable() {
+		super.onDisable();
+		hasToggled=false;
+		
+	}
+	
+	
+	
+	
+	
 }
