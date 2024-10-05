2be17f7f5530db7e449c85877b8cf411833149e8
yuzu-krs <yuzuto.poi@gmail.com>
2024-10-05 13:14:18 +0900

    TABGUIの色変更

--- src/minecraft/me/gamrboy4life/paradox/module/status/TabGui.java
+++ src/minecraft/me/gamrboy4life/paradox/module/status/TabGui.java
@@ -23,13 +23,13 @@ public class TabGui extends Module{
 	
 	public void draw() {
 		if(this.isToggled()) {
-			Gui.drawRect(5, 20, 68, 102, 0x90000000);
-			Gui.drawRect(8, 23+currentTab*13, 7+58, 34+currentTab*13, 0xFF006400);
+			Gui.drawRect(5, 20, 68, 102, 0xAA1E1E1E);
+			Gui.drawRect(8, 23+currentTab*13, 7+58, 34+currentTab*13,0xFF2E8B57);
 			
 			int count=0;
 			
 			for(Category c:Category.values()) {
-				Wrapper.fr.drawString(c.name,10,25+count*13,-1);
+				Wrapper.fr.drawStringWithShadow(c.name,10,25+count*13,0xFFFFFFFF);
 				count++;
 			}
 			
@@ -46,13 +46,13 @@ public class TabGui extends Module{
 					if(Wrapper.fr.getStringWidth(module.name)>maxLenModule)maxLenModule=Wrapper.fr.getStringWidth(module.name);
 				}
 				
-				Gui.drawRect(68, 20, 48+maxLenModule+30, 25+modules.size()*13, 0x90000000);
-				Gui.drawRect(70, 23+category.moduleIndex*13, 63+maxLenModule+12, 34+category.moduleIndex*13,0xFF006400);
+				Gui.drawRect(68, 20, 48+maxLenModule+30, 25+modules.size()*13, 0xAA1E1E1E);
+				Gui.drawRect(70, 23+category.moduleIndex*13, 63+maxLenModule+12, 34+category.moduleIndex*13,0xFF4682B4);
 				
 				count=0;
 				
 				for(Module m:modules) {
-					Wrapper.fr.drawStringWithShadow(m.name,73, 25+count*13, -1 );
+					Wrapper.fr.drawStringWithShadow(m.name,73, 25+count*13, 0xFFFFFFFF );
 					count++;
 				}
 				
