d4cd0a217d4c4d0e472336f4c7fea06b85607492
yuzu-krs <yuzuto.poi@gmail.com>
2024-10-16 03:26:01 +0900

    fillDestroyのundoを追加

--- src/minecraft/me/gamrboy4life/paradox/analysis/UndoDataParser.java
+++ src/minecraft/me/gamrboy4life/paradox/analysis/UndoDataParser.java
@@ -24,6 +24,12 @@ public class UndoDataParser {
 		
 		}else if(isFillReplaceBlock(data)) {
 			executeFillReplaceBlock(data);
+			
+			
+		}else if(isFillDestroy(data)) {
+			executeFillDestroy(data);
+			
+			
 		}else {
 			//Sotuken.instance.moduleManager.addRunChatMessage(data.toString());
 		}
@@ -205,6 +211,40 @@ public class UndoDataParser {
 	    }
 	}
 	
+	private boolean isFillDestroy(String line) {
+		return line.startsWith("999999996,");
+	}
 	
+	
+	//fill系を実行
+		private void executeFillDestroy(String line) {
+			try {
+		        // lineをカンマで分割
+		        String[] parts = line.split(",");
+		        // 9パーツあるか
+		        if (parts.length == 9) {
+		            int x1 = Integer.parseInt(parts[1]);
+		            int y1 = Integer.parseInt(parts[2]);
+		            int z1 = Integer.parseInt(parts[3]);
+		            int x2 = Integer.parseInt(parts[4]);
+		            int y2 = Integer.parseInt(parts[5]);
+		            int z2 = Integer.parseInt(parts[6]);
+		            //String blockName = parts[7];
+		            //int blockInfo = Integer.parseInt(parts[8]);
+
+		            // fillコマンドをマイクラのチャットに送信
+		            Minecraft.getMinecraft().thePlayer.sendQueue.addToSendQueue(
+		                new C01PacketChatMessage(
+		                        String.format("/fill %d %d %d %d %d %d air 0 destroy", 
+		                        x1, y1, z1, x2, y2, z2)
+		                )
+		            );
+		        } else {
+		            Sotuken.instance.moduleManager.addErrRunChatMessage("エラー:fillDestroy関数の形式が違います");
+		        }
+		    } catch (NumberFormatException e) {
+		        Sotuken.instance.moduleManager.addErrRunChatMessage("エラー:fillDestroy関数解析: " + e.getMessage());
+		    }
+		}
 		
 }
